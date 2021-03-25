01691482319a9853aaf9980e9fcdac1cdd39bee5
Codeissimple <adam.jkirjacev@gmail.com>
2021-03-25 14:43:51 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+
+  int inventory_eggs = 42;
+  int inventory_flour = 22;
+  int inventory_milk = 27;
+  int inventory_sugar = 7;
+  int inventory_salt = 10;
+
+  int recipe_eggs = 25;
+  int recipe_flour = 10;
+  int recipe_milk = 5;
+  int recipe_sugar = 1;
+  int recipe_salt = 10;
+  
+  int cake_batch = 50;
+  int desire = 0;
+  float raw_coefficient = 0;
+  int coefficient = 0;
+
+  cout << "Enter the number of cupcakes your heart desires: ";
+  cin >> desire;
+
+  raw_coefficient = desire / cake_batch;
+  
+  
+
+
+  
+  
+  
+}

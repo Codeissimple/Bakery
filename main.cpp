d6d70be3709c22ce7482584118219c9099f90e53
Codeissimple <adam.jkirjacev@gmail.com>
2021-03-25 18:36:42 +0000

    minor

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <math.h>
-
+#include <string> 
 using namespace std;
 
 int main() {
@@ -22,30 +22,36 @@ int main() {
   int raw_coefficient = 0;
   int coefficient = 0;
 
+  double eggs_remainer, flour_remainer,milk_remainer, sugar_remainer, salt_remainer;
+
+  string decline_order = "There is not enough inventory to bake " + std::to_string(desire) + "cupcakes. Sorry!";
+  string accept_order = "We have everything we need to make you " +
+          std::to_string(desire) + " cupcakes! \nThe remainder of our inventory cosists of: "  +
+          "\nEggs: " + std::to_string(eggs_remainer) + "\nFlour: " +
+          std::to_string(flour_remainer) + "\nMilk: "+ std::to_string(milk_remainer) + "\nSugar: " +
+          std::to_string(sugar_remainer) + "\nSalt: "+ std::to_string(salt_remainer);
+  
+  cout << accept_order;
+
   cout << "Enter the number of cupcakes your heart desires: ";
   cin >> desire;
 
   raw_coefficient = desire / cake_batch;
-  
+
   if (raw_coefficient <= 0)
     return 0;
-  else
-    if (inventory_eggs / (recipe_eggs * raw_coefficient) <= 0  )
+  else if (inventory_eggs / (recipe_eggs * raw_coefficient) <= 0  )
       return 0;
-    else
-      if (inventory_flour / recipe_flour * raw_coefficient <= 0)
+    else if (inventory_flour / recipe_flour * raw_coefficient <= 0)
         return 0;
-      else
-        if (inventory_milk / recipe_milk * raw_coefficient <= 0)
+      else if (inventory_milk / recipe_milk * raw_coefficient <= 0)
           return 0;
-        else
-          if (inventory_sugar / recipe_sugar * raw_coefficient <= 0)
+        else if (inventory_sugar / recipe_sugar * raw_coefficient <= 0)
             return 0;
           else if (inventory_salt / recipe_salt * raw_coefficient <= 0)
             return 0;
-          else
+          else if (inventory_eggs / (recipe_eggs * raw_coefficient) > 0)
             cout << "We're making you " << desire;
-
 
     
   

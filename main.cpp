a9cc8d21ca51aef82243653be1e5df4c135dd3bf
Codeissimple <adam.jkirjacev@gmail.com>
2021-03-30 02:30:39 +0000

    New version

--- main.cpp
+++ main.cpp
@@ -5,60 +5,73 @@ using namespace std;
 
 int main() {
 
-  int inventory_eggs = 42;
-  int inventory_flour = 22;
-  int inventory_milk = 27;
-  int inventory_sugar = 7;
-  int inventory_salt = 10;
+  int inventory_eggs = 4;      int recipe_eggs = 25;     int cake_batch = 50;
+  int inventory_flour = 220;   int recipe_flour = 10;    int purchase_order = 0;
+  int inventory_milk = 270;    int recipe_milk = 5;      int overflow = 0;
+  int inventory_sugar = 70;    int recipe_sugar = 1;     double workload = 0;
+  int inventory_salt = 100;    int recipe_salt = 10;     double raw_coefficient = 0;
 
-  int recipe_eggs = 25;
-  int recipe_flour = 10;
-  int recipe_milk = 5;
-  int recipe_sugar = 1;
-  int recipe_salt = 10;
+  int eggs_remainer, flour_remainer,milk_remainer, sugar_remainer, salt_remainer;
+
+  cout << "Enter the number of cupcakes you want: ";
+  cin >> purchase_order;
+
+  if (purchase_order > 50)
+    workload = purchase_order / cake_batch;
+  else
+    workload = purchase_order;
+
+  overflow = purchase_order % cake_batch;
+
+  if (overflow > 0)
+    workload = workload + 50;
+  else
+    workload = workload;
   
-  int cake_batch = 50;
-  int desire = 0;
-  int raw_coefficient = 0;
-  int coefficient = 0;
+cout << workload 
 
-  double eggs_remainer, flour_remainer,milk_remainer, sugar_remainer, salt_remainer;
 
-  string decline_order = "There is not enough inventory to bake " + std::to_string(desire) + "cupcakes. Sorry!";
+
+
+
+/*
+  string decline_order = "There is not enough inventory to bake " + to_string(desire) + " cupcakes. Sorry!";
   string accept_order = "We have everything we need to make you " +
-          std::to_string(desire) + " cupcakes! \nThe remainder of our inventory cosists of: "  +
-          "\nEggs: " + std::to_string(eggs_remainer) + "\nFlour: " +
-          std::to_string(flour_remainer) + "\nMilk: "+ std::to_string(milk_remainer) + "\nSugar: " +
-          std::to_string(sugar_remainer) + "\nSalt: "+ std::to_string(salt_remainer);
-  
-  cout << accept_order;
+          to_string(desire) + " cupcakes! \nThe remainder of our inventory cosists of: "  +
+          "\nEggs: " + to_string(eggs_remainer) + "\nFlour: " +
+          to_string(flour_remainer) + "\nMilk: "+ to_string(milk_remainer) + "\nSugar: " +
+          to_string(sugar_remainer) + "\nSalt: "+ to_string(salt_remainer);
 
   cout << "Enter the number of cupcakes your heart desires: ";
   cin >> desire;
 
+  if (desire % cake_batch == 0)
+    cake_batch = cake_batch;
+  else
+    cake_batch = cake_batch + 50;
+
   raw_coefficient = desire / cake_batch;
+  //cout << raw_coefficient << endl;
+
+  eggs_remainer = inventory_eggs - (recipe_eggs * raw_coefficient);
+  flour_remainer = inventory_flour - (recipe_flour * raw_coefficient);
+  milk_remainer = inventory_milk - (recipe_milk * raw_coefficient);
+  sugar_remainer = inventory_sugar - (recipe_sugar * raw_coefficient);
+  salt_remainer = inventory_salt -(recipe_salt * raw_coefficient);
 
   if (raw_coefficient <= 0)
     return 0;
   else if (inventory_eggs / (recipe_eggs * raw_coefficient) <= 0  )
       return 0;
-    else if (inventory_flour / recipe_flour * raw_coefficient <= 0)
+    else if (inventory_flour / (recipe_flour * raw_coefficient) <= 0)
         return 0;
-      else if (inventory_milk / recipe_milk * raw_coefficient <= 0)
+      else if (inventory_milk / (recipe_milk * raw_coefficient) <= 0)
           return 0;
-        else if (inventory_sugar / recipe_sugar * raw_coefficient <= 0)
+        else if (inventory_sugar / (recipe_sugar * raw_coefficient) <= 0)
             return 0;
-          else if (inventory_salt / recipe_salt * raw_coefficient <= 0)
+          else if (inventory_salt / (recipe_salt * raw_coefficient) <= 0)
             return 0;
-          else if (inventory_eggs / (recipe_eggs * raw_coefficient) > 0)
-            cout << "We're making you " << desire;
-
-    
-  
-
-
-
-  
-  
-  
+          else
+            cout << accept_order;
+*/
 }

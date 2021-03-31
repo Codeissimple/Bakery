c5c0613d9229a885b434b9d22499f6324084924c
Codeissimple <adam.jkirjacev@gmail.com>
2021-03-31 23:53:01 +0000

    2

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main() {
 
-  int inventory_eggs = 48;      int recipe_eggs = 25;    double cake_batch = 50.0;
+  int inventory_eggs = 50;      int recipe_eggs = 25;    double cake_batch = 50.0;
   int inventory_flour = 220;   int recipe_flour = 10;    double purchase_order = 1.0;
   int inventory_milk = 270;    int recipe_milk = 5;      double upsell = 0;
   int inventory_sugar = 70;    int recipe_sugar = 1;     double workload = 0;
@@ -13,15 +13,43 @@ int main() {
 
   int eggs_remainer, flour_remainer,milk_remainer, sugar_remainer, salt_remainer;
 
+
+
+
   cout << "Enter the number of cupcakes you want: ";
   cin >> purchase_order;
+
+  string accept_order = "We have everything we need to make you " +
+          to_string(round(purchase_order)) + " cupcakes! \nThe remainder of our inventory cosists of: "  +
+          "\nEggs: " + to_string(eggs_remainer) + "\nFlour: " +
+          to_string(flour_remainer) + "\nMilk: "+ to_string(milk_remainer) + "\nSugar: " +
+          to_string(sugar_remainer) + "\nSalt: "+ to_string(salt_remainer);
 
   
   workload = purchase_order / cake_batch;
   
 
+  eggs_remainer = inventory_eggs - (recipe_eggs * raw_coefficient);
+  flour_remainer = inventory_flour - (recipe_flour * raw_coefficient);
+  milk_remainer = inventory_milk - (recipe_milk * raw_coefficient);
+  sugar_remainer = inventory_sugar - (recipe_sugar * raw_coefficient);
+  salt_remainer = inventory_salt -(recipe_salt * raw_coefficient);
+
+
+
+  cout << workload << endl;
   
-  cout << ceil(workload);
+  cout << ceil(workload) << endl;
+
+  
+  if (inventory_eggs / (recipe_eggs * ceil(workload)) > 0 
+  && inventory_flour / (recipe_flour * ceil(workload)) > 0 
+  && inventory_milk / (recipe_milk * ceil(workload)) > 0 
+  && inventory_sugar / (recipe_sugar * ceil(workload)) 
+  && inventory_salt / (recipe_salt * ceil(workload)) > 0)
+    cout << accept_order;
+  else
+    return 0;
 
 
 }

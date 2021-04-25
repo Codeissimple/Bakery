d430a92855fc3c5b5da5dc3cf262e51f0f00888e
Codeissimple <>
2021-04-25 16:50:39 +0000

    everything

--- main.cpp
+++ main.cpp
@@ -3,50 +3,45 @@
 #include <string> 
 using namespace std;
 
+const int recipe_eggs = 25;
+const int recipe_flour = 10;
+const int recipe_milk = 5; 
+const int recipe_sugar = 1;
+const int recipe_salt = 10;
+
 int main() {
 
-  int inventory_eggs = 50;      int recipe_eggs = 25;    double cake_batch = 50.0;
-  int inventory_flour = 220;   int recipe_flour = 10;    double purchase_order = 1.0;
-  int inventory_milk = 270;    int recipe_milk = 5;      double upsell = 0;
-  int inventory_sugar = 70;    int recipe_sugar = 1;     double workload = 0;
-  int inventory_salt = 100;    int recipe_salt = 10;     
+  int inventory_eggs = 50;         
+  int inventory_flour = 220;      
+  int inventory_milk = 270;         
+  int inventory_sugar = 70;        
+  int inventory_salt = 130;
 
-  int eggs_remainer, flour_remainer,milk_remainer, sugar_remainer, salt_remainer;
+  double purchase_order = 1.0;
+  double cake_batch = 50.0;  
+  double workload = 0;      
 
+  int eggs_remainer = 0;
+  int flour_remainer = 0;
+  int milk_remainer = 0;
+  int sugar_remainer = 0;
+  int salt_remainer = 0;
 
   cout << "Enter the number of cupcakes you want: ";
   cin >> purchase_order;
 
+  workload = ceil(purchase_order / cake_batch);
 
-  string accept_order = "We have everything we need to make you " +
-          to_string(purchase_order) + " cupcakes! \nThe remainder of our inventory cosists of: "  +
-          "\nEggs: " + to_string(eggs_remainer) + "\nFlour: " +
-          to_string(flour_remainer) + "\nMilk: "+ to_string(milk_remainer) + "\nSugar: " +
-          to_string(sugar_remainer) + "\nSalt: "+ to_string(salt_remainer);
-  string decline_order = "There is not enough inventory to bake " + to_string(purchase_order) + " cupcakes. Sorry!";
+  eggs_remainer = (inventory_eggs - (recipe_eggs * workload));
+  flour_remainer = (inventory_flour - (recipe_flour * workload));
+  milk_remainer = (inventory_milk - (recipe_milk * workload));
+  sugar_remainer = (inventory_sugar - (recipe_sugar * workload));
+  salt_remainer = (inventory_salt - (recipe_salt * workload));
 
-  
-  workload = purchase_order / cake_batch;
-  
-
-  eggs_remainer = inventory_eggs - (recipe_eggs * workload);
-  flour_remainer = inventory_flour - (recipe_flour * workload);
-  milk_remainer = inventory_milk - (recipe_milk * workload);
-  sugar_remainer = inventory_sugar - (recipe_sugar * workload);
-  salt_remainer = inventory_salt -(recipe_salt * workload);
-
-  cout << workload << endl;
-  
-  cout << ceil(workload) << endl;
-
-  
-  if (inventory_eggs / (recipe_eggs * ceil(workload)) > 0 
-  && inventory_flour / (recipe_flour * ceil(workload)) > 0 
-  && inventory_milk / (recipe_milk * ceil(workload)) > 0 
-  && inventory_sugar / (recipe_sugar * ceil(workload)) 
-  && inventory_salt / (recipe_salt * ceil(workload)) > 0)
-    cout << accept_order;
+  if(eggs_remainer < 0 || flour_remainer < 0 || milk_remainer < 0 || sugar_remainer < 0 || salt_remainer < 0)
+    cout << "There is not enough inventory to bake you " << purchase_order << " cupcakes. ";
   else
-    cout << decline_order;
+    cout << "Great! We can make you " << purchase_order << " cupcakes, and we will have remaining: \n" <<
+    "Eggs: " << eggs_remainer << "\n" << "Flour: " << flour_remainer << "\n" << "Milk: " << milk_remainer << "\n" << "Sugar: " << sugar_remainer << "\n" << "Salt: " << salt_remainer << endl;
+return 0;
 }
-
